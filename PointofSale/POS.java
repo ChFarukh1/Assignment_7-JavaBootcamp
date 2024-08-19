f24479954a8078d803cc1a1fa2c73367496c04f0
Choudhary Farukh <119169306+ChFarukh1@users.noreply.github.com>
2024-08-19 13:01:32 +0500

    Create POS.java

---
+++ PointofSale/POS.java
@@ -0,0 +1,35 @@
+public class POS {
+    private Product product;
+    private int quantity;
+    private double price;
+
+    public POS(Product product, int quantity, double price) {
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
